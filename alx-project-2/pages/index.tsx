dd0189adaa75a9ce0455f60d065d5ff614b5c441
khaoularj <khaoula.raja1@gmail.com>
2025-01-09 13:20:29 +0000

    Set Up a Next.js Project

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,10 @@
+import Header from '@/components/layout/Header';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <Header />
+      
+    </div>
+  );
+}
