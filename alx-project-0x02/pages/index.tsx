6914b967e3935c023fb661342f7a60ceb772468a
khaoularj <khaoula.raja1@gmail.com>
2025-01-09 14:26:13 +0000

    Set Up a Next.js Project

--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <Header />
-      
+
     </div>
   );
 }
