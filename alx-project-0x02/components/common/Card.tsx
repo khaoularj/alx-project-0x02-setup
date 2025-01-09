5c3cb14966a48ffb7e0034f3197f7a19a7ad7241
khaoularj <khaoula.raja1@gmail.com>
2025-01-09 15:28:35 +0000

    updating card.tsx

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CardProps } from '../../interfaces/index';
+import { CardProps } from '@/interfaces'
 
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
